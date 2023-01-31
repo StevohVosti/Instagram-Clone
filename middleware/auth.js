0f6b426f4efaee875d430abdc8e98ea8ea0a3037
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-01-31 18:29:38 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,9 +4,9 @@ exports.verifyToken = async (req, res, next) => {
   try {
     let token = req.header("Authorization");
 
-//     if (!token) {
-//       return res.status(403).send("Access Denied");
-//     }
+    if (!token) {
+      return res.status(403).send("Access Denied");
+    }
 
     if (token.startsWith("Bearer ")) {
       token = token.slice(7, token.length).trimLeft();
