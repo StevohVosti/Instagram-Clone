4b7f5643294d3327b533ed1720a1cf3ccdc461a6
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-01-31 16:40:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+dotenv.config({path: "config/config.env"});
 const connectDatabase = require("./Database/db");
 const { default: mongoose } = require("mongoose");
-dotenv.config({path: "config/config.env"});
 
-mongoose.set("strictQuery", false);
+// Connectng to MongoDB
 mongoose.set("strictQuery", true);
-
-// Connect Database
+mongoose.set("strictQuery", false);
 connectDatabase();
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled Promise Rejection
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the Server due to unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
