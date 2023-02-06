6ac3448a6d36292e1b641b9b94cfa0839b9acb1b
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-02-06 12:31:57 +0300

    Adding imports

--- app.js
+++ app.js
@@ -1,34 +1,16 @@
 const express = require("express");
 const app = express();
-const multer = require("multer");
 
 app.use(express.json());
 
 // Imports
 const auth = require("./routes/authRoute");
-const user = require("./routes/usersRoute");
-const posts = require("./routes/postRoute");
-const { register } = require("./controllers/Authcontroller");
-const { createPost } = require("./controllers/postController");
-const { verifyToken } = require("./middleware/auth");
+const users = require("./routes/usersRoute");
+const post  = require("./routes/postRoute");
 
-/* FILE STORAGE */
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/assets");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-const upload = multer({ storage });
-
-/* ROUTES WITH FILES */
-app.post("/register", upload.single("picture"), register);
-app.post("/posts",  upload.single("picture"), createPost);
 
 app.use("/api/v1", auth);
-app.use("/api/v1", user);
-app.use("/api/v1", posts);
+app.use("/api/v1", users);
+app.use("/api/v1", post);
 
 module.exports = app;
