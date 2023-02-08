dbc17074f3cede470fa853366d4f13faae73d602
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-02-08 17:05:11 +0300

    Adding frontend

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+import {
+  CLEAR_ERRORS,
+  LOGIN_FAIL,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  REGISTER_USER_FAIL,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+} from "../constants/userConstants";
+
+// Login
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: LOGIN_REQUEST,
+    });
+
+    const config = { headers: { "Content-Type": "application/json" } };
+
+    const { data } = await axios.post(
+      `/api/v1/login`,
+      { email, password },
+      config
+    );
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGIN_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// Register
+export const register = (userData) => async (dispatch) => {
+  try {
+    dispatch({
+      type: REGISTER_USER_REQUEST,
+    });
+
+    const config = { headers: { "Content-Type": "multipart/form-data" } };
+
+    const { data } = await axios.post(`/api/v1/register`, userData, config);
+
+    dispatch({
+      type: REGISTER_USER_SUCCESS,
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: REGISTER_USER_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// Clearing Errors
+export const clearErrors = () => async (dispatch) => {
+  dispatch({
+    type: CLEAR_ERRORS,
+  });
+};
