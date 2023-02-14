e2b9b88de04ea54ecc9c0eab2c9df6c4a2e0a5d5
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-02-14 16:23:00 +0300

    Adding SendEmail

---
+++ middleware/sendEmail.js
@@ -0,0 +1,26 @@
+const nodeMailer = require("nodemailer");
+const dotenv = require('dotenv');
+dotenv.config({ path: "config/config.env" });
+
+const sendEmail = async (options) => {
+  const transporter = nodeMailer.createTransport({
+    host: process.env.SMPT_HOST,
+    port: 465,
+    service: process.env.SMPT_SERVICE,
+    auth: {
+      user: process.env.SMPT_MAIL,
+      pass: process.env.SMPT_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.SMPT_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
