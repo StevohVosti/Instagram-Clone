72f5ee432380cafac1d20550c49378f786b25b16
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-02-14 16:26:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const dotenv = require("dotenv");
 dotenv.config({path: "config/config.env"});
 const connectDatabase = require("./Database/db");
 const { default: mongoose } = require("mongoose");
+const cloudinary = require("cloudinary");
 
 // Connectng to MongoDB
 mongoose.set("strictQuery", true);
 mongoose.set("strictQuery", false);
 connectDatabase();
+
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
