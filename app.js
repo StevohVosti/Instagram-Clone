583d30dbba3fe476ca15e5396e69000d5f619ffc
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-02-14 17:44:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,4 +13,11 @@ app.use("/api/v1", auth);
 app.use("/api/v1", users);
 app.use("/api/v1", post);
 
+    // SERVING STATIC FILES
+    app.use(express.static("frontend/build"));
+    // SERVE UP INDEX.HTML IF IT DOESNOT RECORGANISE THE ROUTE
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    });
+
 module.exports = app;
