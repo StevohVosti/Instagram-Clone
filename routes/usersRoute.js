dd2efde3173005f4bfbde3f9b1699d53a7765b0e
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-02-14 16:26:23 +0300

    Update usersRoute.js

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,18 +1,48 @@
 const express = require("express");
 const {
-  getAllUsers,
+  register,
+  login,
   followUser,
-  unfollowUser,
-  getFollowers,
-  getUserById,
+  logout,
+  updatePassword,
+  updateProfile,
+  deleteMyProfile,
+  myProfile,
+  getUserProfile,
+  getAllUsers,
+  forgotPassword,
+  resetPassword,
+  getMyPosts,
+  getUserPosts,
 } = require("../controller/userController");
-
+const { isAuthenticated } = require("../middleware/auth");
 const router = express.Router();
 
-router.route("/users").get(getAllUsers);
-router.route("/user/:id").get(getUserById);
-router.route("/followers/:userId").get(getFollowers);
-router.route("/:id/follow").put(followUser);
-router.route("/:id/unfollow").put(unfollowUser);
+router.route("/register").post(register);
+
+router.route("/login").post(login);
+
+router.route("/logout").get(logout);
+
+router.route("/follow/:id").get(isAuthenticated, followUser);
+
+router.route("/update/password").put(isAuthenticated, updatePassword);
+
+router.route("/update/profile").put(isAuthenticated, updateProfile);
+
+router.route("/delete/me").delete(isAuthenticated, deleteMyProfile);
+router.route("/me").get(isAuthenticated, myProfile);
+
+router.route("/my/posts").get(isAuthenticated, getMyPosts);
+
+router.route("/userposts/:id").get(isAuthenticated, getUserPosts);
+
+router.route("/user/:id").get(isAuthenticated, getUserProfile);
+
+router.route("/users").get(isAuthenticated, getAllUsers);
+
+router.route("/forgot/password").post(forgotPassword);
+
+router.route("/password/reset/:token").put(resetPassword);
 
 module.exports = router;
